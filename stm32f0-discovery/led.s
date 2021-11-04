ff5bf453d2c3a0acb9e9236ed5ae5f5504a2a8c1
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-11-04 14:36:58 +0100

    Use /* */ for comments as @ did "work" for me

--- stm32f0-discovery/led.s
+++ stm32f0-discovery/led.s
@@ -24,35 +24,40 @@
 .equ BLUE, 7
 .equ ORANGE, 8
 .equ GREEN, 9
-@ Enable writing for MODER9 (Mode Register) which is the green LED.
+/* Enable writing for MODER9 (Mode Register) which is the green LED.*/
 .equ GPIOC_MODER_MASK_GREEN, 1 << (GREEN * 2)
 .equ LED_MASK_GREEN, 1 << GREEN
 
-@ Enable writing for MODER8 (Mode Register) which is the orange LED.
-@.equ GPIOC_MODER_MASK, 1 << (ORANGE * 2)
-@.equ LED_MASK, 1 << ORANGE
+/* Enable writing for MODER8 (Mode Register) which is the orange LED. */
+/*
+.equ GPIOC_MODER_MASK, 1 << (ORANGE * 2)
+.equ LED_MASK, 1 << ORANGE
+*/
 
-@ Enable writing for MODER7 (Mode Register) which is the blue LED.
+/* Enable writing for MODER7 (Mode Register) which is the blue LED. */
 .equ GPIOC_MODER_MASK_BLUE, 1 << (BLUE * 2)
 .equ LED_MASK_BLUE, 1 << BLUE
 
 .equ GPIOC_MODER_MASK, GPIOC_MODER_MASK_BLUE + GPIOC_MODER_MASK_GREEN
 .equ LED_MASK, LED_MASK_BLUE + LED_MASK_GREEN
 
-@ Enable writing for MODER6 (Mode Register)  which is the red LED.
-@.equ GPIOC_MODER_MASK, 1 << (RED * 2)
-@.equ LED_MASK, 1 << RED
+/* Enable writing for MODER6 (Mode Register)  which is the red LED. */
+/*
+.equ GPIOC_MODER_MASK, 1 << (RED * 2)
+.equ LED_MASK, 1 << RED
+*/
 
 .equ DELAY_LENGTH, 0x000fffff
 
-@ Set up interrupt vector table: 
-@ entry 0: initial stack pointer
-@ entry 1: reset address
+/* Set up interrupt vector table: 
+ entry 0: initial stack pointer
+ entry 1: reset address
+*/
 Vector_Table:    
-  .word     0x20002000 @ stack pointer
+  .word     0x20002000
 
 ResetVector:
-  .word     start + 1  @ reset address
+  .word     start + 1
 
 start:
   ldr r1, =RCC_AHBENR
@@ -86,13 +91,13 @@ delay:
   bx  lr
  
 set_bits: 
-  ldr r0, [r1]   @ deref r1 and store in r0.
-  orr r0, r0, r2 @ OR that with r2 saving back to r0.
-  str r0, [r1]   @ store r0 into r1, updating that r1 points to.
-  bx  lr         @ branch to the address in lr. x changes the processor state
-                 @ to ARM or Thumb depending on the bit[0] of lr. If bit[0] is0
-                 @ 0 then processor changes to ARM state, and if it is 1 will
-                 @ change (or remain) in Thumb state.
+  ldr r0, [r1]   /* deref r1 and store in r0. */
+  orr r0, r0, r2 /* OR that with r2 saving back to r0. */
+  str r0, [r1]   /* store r0 into r1, updating that r1 points to. */
+  bx  lr         /* branch to the address in lr. x changes the processor state*/
+                 /* to ARM or Thumb depending on the bit[0] of lr. If bit[0] is**/
+                 /* 0 then processor changes to ARM state, and if it is 1 will*/
+                 /* change (or remain) in Thumb state. */
 
 clear_bits: 
   ldr r0, [r1]
