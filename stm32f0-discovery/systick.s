37c622feee39bcbd84c807bb209581eb9543b48f
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-11-23 12:50:07 +0100

    Update systick.s to blink using systick timer

--- stm32f0-discovery/systick.s
+++ stm32f0-discovery/systick.s
@@ -3,10 +3,9 @@
 
 /* Reset Clock and Counter register base address */
 .equ RCC_BASE, 0x40021000
-/* Univeral Serial Asynchronous Receiver Transmitter Base address */
-.equ USART1_BASE, 0x40013800
 /* General Purpose IO Port A base register */
 .equ GPIOA_BASE, 0x48000000
+
 /* SysTick Control and Status Register */
 .equ SYST_CSR, 0xE000E010
 /* SysTick Reload Value Register */
@@ -15,18 +14,6 @@
 .equ SYST_CVR, 0xE000E018
 /* SysTick Calibration Value Register */
 .equ SYST_CALIB, 0xE000E01C
-
-/* Advanced Peripheral Bus 1 Enable Register Offset (from RCC_BASE) */
-.equ RCC_APB1ENR_OFFSET, 0x1C
-.equ RCC_APB1ENR, RCC_BASE + RCC_APB1ENR_OFFSET
-
-.equ RCC_APB2ENR_OFFSET, 0x18
-.equ RCC_APB2ENR, RCC_BASE + RCC_APB2ENR_OFFSET
-
-/* Mode Register Offset for Port A (from GPIO_BASE) */
-.equ GPIOA_MODER_OFFSET, 0x00
-/* Mode Register for GPIO Port A */
-.equ GPIOA_MODER, GPIOA_BASE + GPIOA_MODER_OFFSET
 
 /* Advanced High Performace Bus Enable Register offset (from RCC) */
 .equ AHBENR_OFFSET, 0x14
@@ -45,60 +32,65 @@
 /* Enable the timer */
 .equ SYST_CSR_ENABLE, 1 << 0
 /* 0 = external clock source, 1 = processor clock source */
-.equ SYST_CSR_CLK_SOURCE, 1 << 3
+.equ SYST_CSR_CLK_SOURCE, 1 << 2
 /* 0 = does not assert the SysTick exception (interrupt),
    1 = does assert the SysTick exception (interrupt). */
 .equ SYST_CSR_TICKINT, 1 << 1
 /* Determines if the timer has counted down to zero since last read. */
 .equ SYST_CSR_COUNT_FLAG, 1 << 16
-/* 24 bits is the max number of bits for the reload value */
-.equ SYST_RVR_VALUE, 0x00FFFFFF
 
-.equ TEN_MS, 160000
+/* Reload Value */
+.equ SYST_RVR_VALUE, 8000000 - 1
 
 .global start
 
-Vector_Table:    
-  .word     0x20002000
-
-ResetVector:
-  .word     start + 1
+Vector_Table:              /* Vector                       Exception Nr */
+  .word     0x20002000     /* Initial Stack Pointer value             - */
+ResetHandler:              /* Reset                                   1 */
+  .word     start + 1 
 
 start:
   bl gpio_init
   bl systick_init
+
 main_loop:
-  mov r0, #100
-  bl systick_wait_ten_ms
+  bl systick_wait_clock_cycles
   bl turn_led_on
-  bl systick_wait_ten_ms
+  bl systick_wait_clock_cycles
   bl turn_led_off
   b main_loop
 
-/* The passed in argument in r0 is the number of clock cycles to wait */
-systick_wait:
-  ldr r1, =SYST_RVR
-  sub r0, #1
-  str r0, [r1]
+systick_init:
+  /* Clear the SysTick Control Register */
   ldr r1, =SYST_CSR
+  mov r2, #0
+  str r2, [r1]
 
-wait_loop:
-  ldr r3, [r1]
-  ldr r2, =#0x0001
-  and r3, r3, r2
-  beq wait_loop   /* COUNT_FLAG will be 1 only if the timer counted to zero since the last read */
+  /* Reload Value Register configuration */
+  ldr r1, =SYST_RVR
+  ldr r2, =SYST_RVR_VALUE
+  str r2, [r1]
+
+  /* Current Value Register configuration */
+  ldr r1, =SYST_CVR
+  mov r2, #0
+  str r2, [r1]
+
+  /* Control and Status Register configuration */
+  ldr r1, =SYST_CSR
+  mov r2, #(SYST_CSR_CLK_SOURCE + SYST_CSR_ENABLE)
+  str r2, [r1]
+
   bx lr
 
-systick_wait_ten_ms:
-  mov r4, r0 
-  BEQ done
-wait_loop2:
-  ldr r0, =TEN_MS
-  bl systick_wait
-  sub r4, #1
-  bhi wait_loop2
+systick_wait_clock_cycles:
+  ldr r1, =SYST_CSR
 
-done:
+count_flag_wait:
+  ldr r3, [r1]
+  ldr r2, =SYST_CSR_COUNT_FLAG
+  and r3, r3, r2
+  beq count_flag_wait   /* COUNT_FLAG will be 1 only if the timer counted to zero since the last read */
   bx lr
 
 gpio_init:
@@ -117,42 +109,6 @@ gpio_init:
 
   bx lr
 
-systick_init:
-  ldr r1, =SYST_CSR
-  /* start by clearning the SysTick Control Register */
-  mov r0, #0
-  str r0, [r1]
-
-  ldr r1, =SYST_RVR
-  ldr r0, =SYST_RVR_VALUE
-  str r0, [r1]
-
-  /* Current Value Register */
-  ldr r1, =SYST_CVR
-  mov r0, #0
-  str r0, [r1]
-
-  ldr r1, =SYST_CSR
-  mov r0, #(SYST_CSR_CLK_SOURCE + SYST_CSR_ENABLE)
-  str r0, [r1]
-
-/*
-  ldr r2, =SYST_CSR_CLK_SOURCE
-  orr r0, r0, r2
-
-  ldr r2, =SYST_CSR_ENABLE
-  orr r0, r0, r2
-
-  ldr r2, =SYST_CSR_ENABLE
-  orr r0, r0, r2
-
-  ldr r2, =SYST_CSR_TICKINT
-  orr r0, r0, r2
-  str r0, [r1]
-*/
-
-  bx lr
-
 turn_led_on:
   ldr r1,=GPIOC_BSRR
   ldr r2,=BSRR_9_SET
@@ -168,11 +124,3 @@ turn_led_off:
   orr r0, r0, r2
   str r0, [r1]
   bx lr
-
-.equ DELAY_LENGTH, 100000
-delay:
-  ldr r0,=DELAY_LENGTH
-dloop:
-  sub r0, r0, #1
-  bne dloop      /* branch while the Z (zero) flag is not equal to zero */
-  bx  lr
