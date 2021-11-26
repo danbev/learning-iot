40fb69a18371843d101ecea9fc093ce65b063c61
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-11-26 05:52:54 +0100

    Use blue-led.s include in uart-rx-sync.s

--- stm32f0-discovery/uart-rx-sync.s
+++ stm32f0-discovery/uart-rx-sync.s
@@ -200,31 +200,7 @@ tx_output_loop:
   str r3, [r2]
   pop {pc}
 
-.equ GPIO_PORTC_ENABLE, 1 << 19
-.equ GPIOC_MODER_MASK, 1 << 14
-.equ GPIOC_MODER_OFFSET, 0x00
-.equ GPIOC_MODER, GPIOC_BASE + GPIOC_MODER_OFFSET
-.equ GPIOC_BASE, 0x48000800
-.equ GPIOC_BSRR_OFFSET, 0x18
-.equ GPIOC_BSRR, GPIOC_BASE + GPIOC_BSRR_OFFSET
-.equ BSRR_9_SET, 1 << 7
-.equ BSRR_9_RESET, 1 << 23
-
-turn_led_on:
-  ldr r1,=GPIOC_BSRR
-  ldr r2,=BSRR_9_SET
-  ldr r0, [r1]
-  orr r0, r0, r2
-  str r0, [r1]
-  bx lr
-
-turn_led_off:
-  ldr r1,=GPIOC_BSRR
-  ldr r2,=BSRR_9_RESET
-  ldr r0, [r1]
-  orr r0, r0, r2
-  str r0, [r1]
-  bx lr
+.include "blue-led.s"
 
 .equ DELAY_LENGTH, 100000
 delay:
