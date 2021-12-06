df5757c55be2ca214e31c320ac45afd23b04a9c7
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-12-06 17:46:08 +0100

    Clean up led_button example

--- stm32f0-discovery/led_button.s
+++ stm32f0-discovery/led_button.s
@@ -14,22 +14,7 @@
 /* Advanced High Performace Bus Enable Register offset (from RCC) */
 .equ AHBENR_OFFSET, 0x14
 .equ RCC_AHBENR, RCC_BASE + AHBENR_OFFSET
-.equ GPIO_PORTC_ENABLE, 1 << 19
 .equ GPIO_PORTA_ENABLE, 1 << 17
-
-/* Mode Register Offset for Port C (from RCC) */
-.equ GPIOC_MODER_OFFSET, 0x00
-/* Mode Register for GPIO Port C */
-.equ GPIOC_MODER, GPIOC_BASE + GPIOC_MODER_OFFSET
-
-/* BSRR Register */
-.equ GPIOC_BSRR_OFFSET, 0x18
-.equ GPIOC_BSRR, GPIOC_BASE + GPIOC_BSRR_OFFSET
-.equ BSRR_9_SET, 1 << 7
-.equ BSRR_9_RESET, 1 << 23
-
-/* Enable writing for MODER9 (Mode Register) which is the green LED */
-.equ GPIOC_MODER_MASK, 1 << 14
 
 /* Input Data Register offset (from GPIOA_BASE) */
 .equ GPIOA_IDR_OFFSET, 0x10
@@ -49,17 +34,7 @@ ResetVector:
                                // that will clear the Thumb state and will
                                // result in a fault of lockup.
 start:
-  ldr r1, =RCC_AHBENR
-  ldr r2, =GPIO_PORTC_ENABLE
-  ldr r0, [r1]
-  orr r0, r0, r2
-  str r0, [r1]
-
-  ldr r1, =GPIOC_MODER
-  ldr r2, =GPIOC_MODER_MASK
-  ldr r0, [r1]
-  orr r0, r0, r2
-  str r0, [r1]
+  bl led_init
 
   ldr r1, =RCC_AHBENR
   ldr r2, =GPIO_PORTA_ENABLE
@@ -77,22 +52,6 @@ main_loop:
   cmp r0, #BTN_OFF
   BEQ turn_led_off
   b main_loop
-
-turn_led_on:
-  ldr r1,=GPIOC_BSRR
-  ldr r2,=BSRR_9_SET
-  ldr r0, [r1]
-  orr r0, r0, r2
-  str r0, [r1]
-  bx lr
-
-turn_led_off:
-  ldr r1,=GPIOC_BSRR
-  ldr r2,=BSRR_9_RESET
-  ldr r0, [r1]
-  orr r0, r0, r2
-  str r0, [r1]
-  bx lr
 
 get_input:
   ldr r1, =GPIOA_IDR
