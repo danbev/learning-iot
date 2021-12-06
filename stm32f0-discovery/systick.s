cd8f1d5f3781de68938d200d5acccb315cb9e1ab
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-12-06 17:23:22 +0100

    Remove unused functions and symbols

--- stm32f0-discovery/systick.s
+++ stm32f0-discovery/systick.s
@@ -17,16 +17,6 @@
 .equ AHBENR_OFFSET, 0x14
 .equ RCC_AHBENR, RCC_BASE + AHBENR_OFFSET
 
-.equ GPIO_PORTC_ENABLE, 1 << 19
-.equ GPIOC_MODER_MASK, 1 << 14
-.equ GPIOC_MODER_OFFSET, 0x00
-.equ GPIOC_MODER, GPIOC_BASE + GPIOC_MODER_OFFSET
-.equ GPIOC_BASE, 0x48000800
-.equ GPIOC_BSRR_OFFSET, 0x18
-.equ GPIOC_BSRR, GPIOC_BASE + GPIOC_BSRR_OFFSET
-.equ BSRR_9_SET, 1 << 7
-.equ BSRR_9_RESET, 1 << 23
-
 /* Enable the timer */
 .equ SYST_CSR_ENABLE, 1 << 0
 /* 0 = external clock source, 1 = processor clock source */
@@ -89,20 +79,4 @@ count_flag_wait:
   ldr r2, =SYST_CSR_COUNT_FLAG
   and r3, r3, r2
   beq count_flag_wait   /* COUNT_FLAG will be 1 only if the timer counted to zero since the last read */
-  bx lr
-
-turn_led_on:
-  ldr r1,=GPIOC_BSRR
-  ldr r2,=BSRR_9_SET
-  ldr r0, [r1]
-  orr r0, r0, r2
-  str r0, [r1]
-  bx lr
-
-turn_led_off:
-  ldr r1,=GPIOC_BSRR
-  ldr r2,=BSRR_9_RESET
-  ldr r0, [r1]
-  orr r0, r0, r2
-  str r0, [r1]
   bx lr
