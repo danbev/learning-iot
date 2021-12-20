9ce191cfa9848e957c6873c1a22cc0972bb7d6eb
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-12-20 14:23:32 +0100

    Add CAN init code

--- stm32f0-discovery/can-c.s
+++ stm32f0-discovery/can-c.s
@@ -4,8 +4,29 @@ Controller Area Network (CAN) Controller.
 .thumb
 .text
 
-.equ CAN_MCR_OFFSET, 0x00
+.equ CAN_MCR_OFFSET, 0x00       /* Master Control Register                  */
+.equ CAN_MSR_OFFSET, 0x04       /* Master Status Register                   */
+.equ CAN_TSR_OFFSET, 0x08       /* Transmit Status Register                 */
+.equ CAN_ESR_OFFSET, 0x18       /* Error Status Register                    */
+.equ CAN_BTR_OFFSET, 0x1C       /* Bit Timing Register                      */
+.equ CAN_TI0R_OFFSET, 0x180     /* TX Identifier register                   */
+.equ CAN_TDT0R_OFFSET, 0x184    /* TX Data length control register          */
+.equ CAN_TDL0R_OFFSET, 0x188    /* TX Data low register                     */
+.equ CAN_FMR_OFFSET, 0x200      /* Filter Master Register                   */
+
 .equ CAN_MCR, CAN_BASE + CAN_MCR_OFFSET
+.equ CAN_MSR, CAN_BASE + CAN_MSR_OFFSET
+.equ CAN_TSR, CAN_BASE + CAN_TSR_OFFSET
+.equ CAN_ESR, CAN_BASE + CAN_ESR_OFFSET
+.equ CAN_BTR, CAN_BASE + CAN_BTR_OFFSET
+
+.equ CAN_MCR_INRQ, 1 << 0        /* Initialization mode request             */
+.equ CAN_MCR_SLEEP, 1 << 1       /* Sleep mode                              */
+.equ CAN_MSR_INAK, 1 << 0        /* Initialization acknowledgement          */
+.equ CAN_BTR_LBKM, 1 << 30       /* Loopback mode                           */
+.equ CAN_BTR_TS2, 2 << 20        /* TODO:                                   */
+.equ CAN_BTR_TS1, 3 << 16        /* TODO:                                   */
+.equ CAN_BTR_BRP, 3 << 16        /* Baud Rate Prescalar                     */
 
 .global start
 
@@ -19,4 +40,65 @@ start:
   b .
 
 can_controller_init:
+  ldr r1, =CAN_MCR
+  ldr r2, =CAN_MCR_INRQ
+  ldr r0, [r1]
+  orr r0, r0, r2
+  str r0, [r1]
+
+  ldr r1, =CAN_MCR
+  ldr r2, =CAN_MSR_INAK
+wait_inak_set:
+  ldr r0, [r1]
+  and r0, r0, r2
+  cmp r0, r2
+  bne wait_inak_set
+
+  ldr r1, =CAN_MCR
+  ldr r2, =CAN_MCR_INRQ
+  ldr r0, [r1]
+  orr r0, r0, r2
+  str r0, [r1]
+
+  /* Exit sleep mode */
+  ldr r1, =CAN_MCR
+  ldr r2, =CAN_MCR_SLEEP
+  ldr r0, [r1]
+  bic r0, r0, r2
+  str r0, [r1]
+
+  /* Set loopback mode */
+  ldr r1, =CAN_BTR
+  ldr r2, =CAN_BTR_LBKM
+  ldr r0, [r1]
+  orr r0, r0, r2
+  str r0, [r1]
+
+  ldr r1, =CAN_BTR
+  ldr r2, =CAN_BTR_LBKM
+  ldr r0, [r1]
+  orr r0, r0, r2
+  str r0, [r1]
+
+  ldr r1, =CAN_BTR
+  ldr r2, =(CAN_BTR_TS1 + CAN_BTR_TS2 + CAN_BTR_BRP)
+  ldr r0, [r1]
+  orr r0, r0, r2
+  str r0, [r1]
+
+  /* Exit init mode */
+  ldr r1, =CAN_MCR
+  ldr r2, =CAN_MCR_INRQ
+  ldr r0, [r1]
+  bic r0, r0, r2
+  str r0, [r1]
+
+  ldr r1, =CAN_MCR
+  ldr r2, =CAN_MSR_INAK
+wait_inak:
+  ldr r0, [r1]
+  and r0, r0, r2
+  cmp r0, r2
+  beq wait_inak
+
   bx lr
