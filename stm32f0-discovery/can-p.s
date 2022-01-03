445c7e58236e0f2634a8f7542cd81f89fe500e5c
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-01-03 15:28:42 +0100

    Remove unused symbols from can-p.s

--- stm32f0-discovery/can-p.s
+++ stm32f0-discovery/can-p.s
@@ -13,20 +13,20 @@ Controller Area Network (CAN) Controller peripheral example.
 .equ CAN_FM1R_OFFSET, 0x204     /* Filter Mode Register                     */
 .equ CAN_FA1R_OFFSET, 0x21C     /* Filter Activation Register               */
 .equ CAN_FFA1R_OFFSET, 0x214    /* Filter FIFO Assignment Register          */
-.equ CAN_ESR_OFFSET, 0x18        /* Error Status Register                    */
-
-.equ CAN_TI0R_OFFSET, 0x180     /* Transmit Mailbox Identifier 0 Register   */
-.equ CAN_TDT0R_OFFSET, 0x184    /* Transmit Mailbox Data Time 0 Register    */
-.equ CAN_TDL0R_OFFSET, 0x188    /* Transmit Mailbox Data Low 0 Register     */
-.equ CAN_TDH0R_OFFSET, 0x18C    /* Transmit Mailbox Data High 0 Register    */
+.equ CAN_ESR_OFFSET, 0x18       /* Error Status Register                    */
+.equ CAN_RI0R_OFFSET, 0x1B0     /* Receive Identifier 0 Register            */
+.equ CAN_RF0R_OFFSET, 0x0C      /* FIFO Receive 0 Register                  */
+.equ CAN_RF1R_OFFSET, 0x10
+.equ CAN_RDL0R_OFFSET, 0x1B8    /* Receive Data FIFO Low 0 Register         */
 .equ CAN_FS1R_OFFSET, 0x20C     /* Filter Scale Register                    */
-
+.equ CAN_F0R0_OFFSET, 0x240     /* Filter bank 0, register 0                */
+.equ CAN_F0R1_OFFSET, 0x248     /* Filter bank 0, register 1                */
+.equ CAN_FS1R_OFFSET, 0x20C     /* Filter Scale Register                    */
 .equ CAN_F0R0_OFFSET, 0x240     /* Filter bank 0, register 0                */
 .equ CAN_F0R1_OFFSET, 0x248     /* Filter bank 0, register 1                */
 
 .equ CAN_MCR, CAN_BASE + CAN_MCR_OFFSET
 .equ CAN_MSR, CAN_BASE + CAN_MSR_OFFSET
-.equ CAN_TSR, CAN_BASE + CAN_TSR_OFFSET
 .equ CAN_ESR, CAN_BASE + CAN_ESR_OFFSET
 .equ CAN_BTR, CAN_BASE + CAN_BTR_OFFSET
 .equ CAN_FMR, CAN_BASE + CAN_FMR_OFFSET
@@ -39,63 +39,22 @@ Controller Area Network (CAN) Controller peripheral example.
 .equ CAN_FFA1R, CAN_BASE + CAN_FFA1R_OFFSET
 .equ CAN_FS1R, CAN_BASE + CAN_FS1R_OFFSET
 
-.equ CAN_TI0R, CAN_BASE + CAN_TI0R_OFFSET
-.equ CAN_TDT0R, CAN_BASE + CAN_TDT0R_OFFSET
-.equ CAN_TDL0R, CAN_BASE + CAN_TDL0R_OFFSET
+.equ CAN_RI0R, CAN_BASE + CAN_RI0R_OFFSET
+.equ CAN_RF0R, CAN_BASE + CAN_RF0R_OFFSET
+.equ CAN_RF1R, CAN_BASE + CAN_RF1R_OFFSET
+.equ CAN_RDL0R, CAN_BASE + CAN_RDL0R_OFFSET
 
 .equ CAN_MCR_INRQ, 1 << 0        /* Initialization mode request             */
 .equ CAN_MCR_SLEEP, 1 << 1       /* Sleep mode                              */
 .equ CAN_MCR_NART, 1 << 4        /* Autmatic retransmit                     */
 .equ CAN_MSR_INAK, 1 << 0        /* Initialization acknowledgement          */
 .equ CAN_BTR_LBKM, 0 << 30       /* Loopback mode                           */
-.equ CAN_BTR_SJW, 0x1 << 24       /* Loopback mode                           */
-.equ CAN_BTR_TS1, 13 << 16        /* Time Segment 1 (prop seg+phase 1 seg)   */
+.equ CAN_BTR_SJW, 0x1 << 24      /* ReSynchronization Jump Width            */
+.equ CAN_BTR_TS1, 13 << 16       /* Time Segment 1 (prop seg+phase 1 seg)   */
 .equ CAN_BTR_TS2, 2 << 20        /* Time Segment 2 (phase 2 segment)        */
 .equ CAN_BTR_BRP, 6 << 0         /* Baud Rate Prescalar                     */
 .equ CAN_FMR_INIT, 1 << 0        /* Filter init mode                        */
 .equ CAN_FM1R_MASK_MODE, 0 << 0  /* Filter Mask mode                        */
-.equ CAN_FA1R_FACT0, 1 << 0      /* Activate Filter 0                       */
-.equ CAN_F0R0_IDENT, 7 << 21     /* Identifier (currently ignored)          */
-.equ CAN_F0R1_MASK, 0x0000 << 0  /* Filter mask, allow all identifiers      */
-.equ CAN_FFA1R_FFA0, 0 << 0      /* FIFO 0 for Filter 0                     */
-.equ CAN_FFA1R_FFA1, 0 << 1      /* FIFO 0 for Filter 1                     */
-.equ CAN_TSR_TME0, 1 << 26       /* Transmit Mailbox 0 Empty                */
-.equ CAN_TI0R_IDE, 0 << 2        /* Identifier extension                    */
-.equ CAN_TI0R_TXRQ, 1 << 0       /* Transmit request                        */
-.equ CAN_TDT0R_DCL, 1 << 0       /* Data Code Length                        */
-.equ CAN_TI0R_ID, 7 << 21        /* Identifier                              */
-.equ CAN_TI0R_RTR, 1 << 1        /* Data Frame                              */
-.equ CAN_FS1R_FSC0, 1 << 0
-.equ CAN_FS1R_FSC1, 1 << 1
-
-.equ CAN_RI0R_OFFSET, 0x1B0     /* Receive Identifier 0 Register            */
-.equ CAN_RF0R_OFFSET, 0x0C      /* FIFO Receive 0 Register                  */
-.equ CAN_RF1R_OFFSET, 0x10
-.equ CAN_RDL0R_OFFSET, 0x1B8    /* Receive Data FIFO Low 0 Register         */
-
-.equ CAN_FS1R_OFFSET, 0x20C     /* Filter Scale Register                    */
-
-.equ CAN_F0R0_OFFSET, 0x240     /* Filter bank 0, register 0                */
-.equ CAN_F0R1_OFFSET, 0x248     /* Filter bank 0, register 1                */
-
-
-.equ CAN_RI0R, CAN_BASE + CAN_RI0R_OFFSET
-.equ CAN_RF0R, CAN_BASE + CAN_RF0R_OFFSET
-.equ CAN_RF1R, CAN_BASE + CAN_RF1R_OFFSET
-.equ CAN_RDL0R, CAN_BASE + CAN_RDL0R_OFFSET
-.equ CAN_ESR, CAN_BASE + CAN_ESR_OFFSET
-
-
-.equ CAN_MCR_INRQ, 1 << 0        /* Initialization mode request             */
-.equ CAN_MCR_SLEEP, 1 << 1       /* Sleep mode                              */
-.equ CAN_MSR_INAK, 1 << 0        /* Initialization acknowledgement          */
-.equ CAN_BTR_LBKM, 0 << 30       /* Loopback mode                           */
-.equ CAN_BTR_TS1, 13 << 16        /* Time Segment 1 (prop seg+phase 1 seg)   */
-.equ CAN_BTR_TS2, 2 << 20        /* Time Segment 2 (phase 2 segment)        */
-.equ CAN_BTR_BRP, 6 << 0         /* Baud Rate Prescalar                     */
-.equ CAN_FMR_INIT, 1 << 0        /* Filter init mode                        */
-.equ CAN_FM1R_MASK_MODE, 0 << 0  /* Filter Mask mode                        */
-.equ CAN_BTR_SJW, 0x1 << 24       /* Loopback mode                           */
 .equ CAN_FA1R_FACT0, 1 << 0      /* Activate Filter 0                       */
 .equ CAN_F0R0_IDENT, 7 << 21     /* Identifier (currently ignored)          */
 .equ CAN_F0R1_MASK, 0x0000 << 0  /* Filter mask, allow all identifiers      */
@@ -107,16 +66,9 @@ Controller Area Network (CAN) Controller peripheral example.
 .equ CAN_TDT0R_DCL, 1 << 0       /* Data Code Length                        */
 .equ CAN_TI0R_ID, 7 << 21        /* Identifier                              */
 .equ CAN_RF0R_FMP0, 3 << 0       /* Receive FIFO Messsage Pending Mask (11) */
-.equ CAN_RF0R_RFOM0, 1 << 5      /* Release the output mailbox ofthe  FIFO  */
+.equ CAN_RF0R_RFOM0, 1 << 5      /* Release the output mailbox of the FIFO  */
 .equ CAN_FS1R_FSC0, 1 << 0
 .equ CAN_FS1R_FSC1, 1 << 1
-
-.equ CAN_RI0R_STID_MASK, 0xFFE000 /* Recieve Identifier 0 Standard Id Mask   */
-/* 1111 1111 1110 0000 0000 0000
-    15   15  14    0    0    0
-    F    F   E     0    0    0
-   0xFFE << 21
-*/
 
 .global start
 
@@ -271,6 +223,7 @@ can_receive:
   add r0, #48
   bl uart_write_char
 
+  /* Release the output mailbox of the FIFO */
   ldr r1, =CAN_RF0R
   ldr r2, =CAN_RF0R_RFOM0
   ldr r0, [r1]
