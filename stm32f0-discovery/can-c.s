116194e5ecdf82d84759af1aad7aa084e094b599
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-01-03 09:21:16 +0100

    Revert "Remove unused symbols and cleanup of can-c.s"
    This reverts commit 5c1cd8325afeebbe9de3d95fa39f32c1dabd341c.

--- stm32f0-discovery/can-c.s
+++ stm32f0-discovery/can-c.s
@@ -9,17 +9,35 @@ Controller Area Network (CAN) Controller.
 .equ CAN_TSR_OFFSET, 0x08       /* Transmit Status Register                 */
 .equ CAN_ESR_OFFSET, 0x18       /* Error Status Register                    */
 .equ CAN_BTR_OFFSET, 0x1C       /* Bit Timing Register                      */
+.equ CAN_FMR_OFFSET, 0x200      /* Filter Master Register                   */
+.equ CAN_FM1R_OFFSET, 0x204     /* Filter Mode Register                     */
+.equ CAN_FA1R_OFFSET, 0x21C     /* Filter Activation Register               */
+.equ CAN_FFA1R_OFFSET, 0x214    /* Filter FIFO Assignment Register          */
+.equ CAN_ESR_OFFSET, 0x18        /* Error Status Register                    */
 
 .equ CAN_TI0R_OFFSET, 0x180     /* Transmit Mailbox Identifier 0 Register   */
 .equ CAN_TDT0R_OFFSET, 0x184    /* Transmit Mailbox Data Time 0 Register    */
 .equ CAN_TDL0R_OFFSET, 0x188    /* Transmit Mailbox Data Low 0 Register     */
 .equ CAN_TDH0R_OFFSET, 0x18C    /* Transmit Mailbox Data High 0 Register    */
+.equ CAN_FS1R_OFFSET, 0x20C     /* Filter Scale Register                    */
+
+.equ CAN_F0R0_OFFSET, 0x240     /* Filter bank 0, register 0                */
+.equ CAN_F0R1_OFFSET, 0x248     /* Filter bank 0, register 1                */
 
 .equ CAN_MCR, CAN_BASE + CAN_MCR_OFFSET
 .equ CAN_MSR, CAN_BASE + CAN_MSR_OFFSET
 .equ CAN_TSR, CAN_BASE + CAN_TSR_OFFSET
 .equ CAN_ESR, CAN_BASE + CAN_ESR_OFFSET
 .equ CAN_BTR, CAN_BASE + CAN_BTR_OFFSET
+.equ CAN_FMR, CAN_BASE + CAN_FMR_OFFSET
+.equ CAN_FM1R, CAN_BASE + CAN_FM1R_OFFSET
+.equ CAN_FA1R, CAN_BASE + CAN_FA1R_OFFSET
+
+.equ CAN_F0R0, CAN_BASE + CAN_F0R0_OFFSET
+.equ CAN_F0R1, CAN_BASE + CAN_F0R1_OFFSET
+
+.equ CAN_FFA1R, CAN_BASE + CAN_FFA1R_OFFSET
+.equ CAN_FS1R, CAN_BASE + CAN_FS1R_OFFSET
 
 .equ CAN_TI0R, CAN_BASE + CAN_TI0R_OFFSET
 .equ CAN_TDT0R, CAN_BASE + CAN_TDT0R_OFFSET
@@ -30,16 +48,25 @@ Controller Area Network (CAN) Controller.
 .equ CAN_MCR_NART, 1 << 4        /* Autmatic retransmit                     */
 .equ CAN_MSR_INAK, 1 << 0        /* Initialization acknowledgement          */
 .equ CAN_BTR_LBKM, 0 << 30       /* Loopback mode                           */
-.equ CAN_BTR_SJW, 0x1 << 24      /* Loopback mode                           */
-.equ CAN_BTR_TS1, 13 << 16       /* Time Segment 1 (prop seg+phase 1 seg)   */
+.equ CAN_BTR_SJW, 0x1 << 24       /* Loopback mode                           */
+.equ CAN_BTR_TS1, 13 << 16        /* Time Segment 1 (prop seg+phase 1 seg)   */
 .equ CAN_BTR_TS2, 2 << 20        /* Time Segment 2 (phase 2 segment)        */
 .equ CAN_BTR_BRP, 6 << 0         /* Baud Rate Prescalar                     */
+.equ CAN_FMR_INIT, 1 << 0        /* Filter init mode                        */
+.equ CAN_FM1R_MASK_MODE, 0 << 0  /* Filter Mask mode                        */
+.equ CAN_FA1R_FACT0, 1 << 0      /* Activate Filter 0                       */
+.equ CAN_F0R0_IDENT, 7 << 21     /* Identifier (currently ignored)          */
+.equ CAN_F0R1_MASK, 0x0000 << 0  /* Filter mask, allow all identifiers      */
+.equ CAN_FFA1R_FFA0, 0 << 0      /* FIFO 0 for Filter 0                     */
+.equ CAN_FFA1R_FFA1, 0 << 1      /* FIFO 0 for Filter 1                     */
 .equ CAN_TSR_TME0, 1 << 26       /* Transmit Mailbox 0 Empty                */
 .equ CAN_TI0R_IDE, 0 << 2        /* Identifier extension                    */
 .equ CAN_TI0R_TXRQ, 1 << 0       /* Transmit request                        */
 .equ CAN_TDT0R_DCL, 1 << 0       /* Data Code Length                        */
 .equ CAN_TI0R_ID, 7 << 21        /* Identifier                              */
 .equ CAN_TI0R_RTR, 1 << 1        /* Data Frame                              */
+.equ CAN_FS1R_FSC0, 1 << 0
+.equ CAN_FS1R_FSC1, 1 << 1
 
 .global start
 
@@ -59,14 +86,12 @@ send_loop:
   b .
 
 can_controller_init:
-  /* Set CAN Initialization Request to enter initialization mode */
   ldr r1, =CAN_MCR
   ldr r2, =CAN_MCR_INRQ
   ldr r0, [r1]
   orr r0, r0, r2
   str r0, [r1]
 
-  /* Wait for the Initialization Acknowledgement */
   ldr r1, =CAN_MSR
   ldr r2, =CAN_MSR_INAK
 wait_inak_set:
@@ -91,7 +116,6 @@ wait_inak_set:
   str r0, [r1]
 */
 
-  /* Clear Bit Timing Register */
   ldr r1, =CAN_BTR
   ldr r2, =#0x0000
   str r2, [r1]
@@ -103,14 +127,12 @@ wait_inak_set:
   orr r0, r0, r2
   str r0, [r1]
 
-  /* Set the reSynchronization Jump Width */
   ldr r1, =CAN_BTR
   ldr r2, =CAN_BTR_SJW
   ldr r0, [r1]
   orr r0, r0, r2
   str r0, [r1]
 
-  /* Set Time Segment 1/2 and Baud Rate Prescalar */
   ldr r1, =CAN_BTR
   ldr r2, =(CAN_BTR_TS1 + CAN_BTR_TS2 + CAN_BTR_BRP)
   ldr r0, [r1]
@@ -124,9 +146,9 @@ wait_inak_set:
   bic r0, r0, r2
   str r0, [r1]
 
-  /* Wait for Initialization Acknowledgement mode exit */
   ldr r1, =CAN_MSR
   ldr r2, =CAN_MSR_INAK
+  ldr r7, =CAN_ESR
 wait_inak:
   ldr r0, [r1]
   and r0, r0, r2
@@ -136,6 +158,8 @@ wait_inak:
   bx lr
 
 can_send:
+  ldr r6, =CAN_TSR
+  ldr r7, =CAN_ESR
   /* Check that the Transmit 0 mailbox is empty */
   ldr r1, =CAN_TSR
   ldr r2, =CAN_TSR_TME0
@@ -143,6 +167,10 @@ can_send:
   and r0, r0, r2
   cmp r0, r2
   bne mailbox_0_not_empty
+
+  ldr r1, =CAN_TI0R
+  ldr r2, =0x0000
+  str r2, [r1]
 
   /* Set standard message type */
   ldr r1, =CAN_TI0R
@@ -186,6 +214,10 @@ can_send:
   orr r0, r0, r2
   str r0, [r1]
 
+  ldr r4, =CAN_MSR
+  ldr r5, =CAN_TDL0R
+  ldr r7, =CAN_ESR
+ 
   bx lr
 
 mailbox_0_not_empty:
