e3efa56baf51b1dad5f075ed6fc145435ddaf932
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-01-04 13:52:51 +0100

    Add missing enabling of Port A to led-external

--- stm32f0-discovery/led/led-ext.s
+++ stm32f0-discovery/led/led-ext.s
@@ -2,6 +2,10 @@
 .thumb
 .text
 
+.equ RCC_BASE, 0x40021000
+.equ AHBENR_OFFSET, 0x14
+.equ RCC_AHBENR, RCC_BASE + AHBENR_OFFSET
+
 .equ GPIOA_BASE, 0x48000000
 
 .equ GPIOA_MODER_OFFSET, 0x00
@@ -22,6 +26,7 @@
 .equ GPIOA_SPEEDR_OFFSET, 0x08
 .equ GPIOA_SPEEDR, GPIOA_BASE + GPIOA_SPEEDR_OFFSET
 
+.equ GPIO_PORTA_ENABLE, 1 << 17
 .equ GPIOA_MODER_PA4, 1 << 8
 .equ GPIOA_OTYPER_PA4, 0 << 4
 .equ GPIOA_SPEEDR_PA4, 1 << 8
@@ -36,7 +41,6 @@ Vector_Table:
   .word     start + 1
 
 start:
-  bl uart_init
   bl pull_up_pin_init
 
 main_loop:
@@ -48,20 +52,24 @@ main_loop:
   orr r0, r0, r2
   str r0, [r1]
 
-/*
-  ldr r1, =GPIOA_IDR
-  ldr r0, [r1]
-  ldr r2, =GPIOA_IDR_PA4
-  and r0, r0, r2
-  lsr r0, #4
-  bl uart_write_char
-
   bl delay
-*/
+
+  ldr r1, =GPIOA_ODR
+  ldr r2, =GPIOA_ODR_PA4
+  ldr r0, [r1]
+  bic r0, r0, r2
+  str r0, [r1]
   
   b main_loop
 
 pull_up_pin_init:
+  /* Enable Port A clock */
+  ldr r1, =RCC_AHBENR
+  ldr r2, =GPIO_PORTA_ENABLE
+  ldr r0, [r1]
+  orr r0, r0, r2 
+  str r0, [r1]
+
   ldr r1, =GPIOA_MODER
   ldr r2, =GPIOA_MODER_PA4
   ldr r0, [r1]
