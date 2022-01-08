32a84ffee4b8aa1ebe50ad301a1c149223cd9746
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-01-08 13:46:13 +0100

    Update drogue-led example to use drogue-device

--- drogue-led/src/main.rs
+++ drogue-led/src/main.rs
@@ -4,14 +4,19 @@
 
 use embassy::executor::Spawner;
 use embassy_stm32::Peripherals;
-use embedded_hal::digital::v2::OutputPin;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use drogue_device::traits::led::Led as TraitLed;
+use embassy_stm32::peripherals::{PA4};
+use drogue_device::drivers::led::{ActiveHigh, Led};
 use panic_halt as _;
+
+pub type PINPA4 = Output<'static, PA4>;
+pub type LEDPA4 = Led<PINPA4, ActiveHigh>;
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) -> ! {
-    let mut led = Output::new(p.PA4, Level::High, Speed::Low);
-    led.set_high().unwrap();
+    let mut led: LEDPA4 = Led::new(Output::new(p.PA4, Level::High, Speed::Low));
+    led.on().unwrap();
     loop {
     }
 }
