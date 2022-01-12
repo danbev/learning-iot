62dd32a42f5e16297ebb09e74457feaea1bda28b
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-01-12 08:55:26 +0100

    Update embassy-led example with USART example

--- embassy-led/src/main.rs
+++ embassy-led/src/main.rs
@@ -6,13 +6,25 @@ use embassy::executor::Spawner;
 use embassy_stm32::Peripherals;
 use embedded_hal::digital::v2::OutputPin;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::usart::{Config, Uart};
+use embassy_stm32::dma::NoDma;
+use embedded_hal::blocking::serial::Write;
 use panic_halt as _;
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) -> ! {
+    let config = Config::default();
+    let tx = p.PA9;
+    let rx = p.PA10;
+    let mut usart = Uart::new(p.USART1, rx, tx, NoDma, NoDma, config);
+
     let mut led = Output::new(p.PA4, Level::High, Speed::Low);
     led.set_high().unwrap();
+
+    usart.bwrite_all(b"embassy example\r\n").unwrap();
+
     loop {
+
     }
 }
 
