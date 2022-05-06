d1b3ba8a3be435d395165606ec5abeb65ea566ce
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-05-06 09:17:05 +0200

    Remove unused code

--- rp/led/led.s
+++ rp/led/led.s
@@ -8,14 +8,10 @@
 .equ GPIO_25_CTRL_R, USER_BANK0_BASE + 0x0CC // Control register for Pin 25
 
 .equ GPIO_BASE, 0xd0000000                   // SIO base
-.equ GPIO_OE_R, GPIO_BASE + 0x020           // GPIO Output Enable Set Register
 .equ GPIO_OUT_R, GPIO_BASE + 0x010          // GPIO Output Register
-
-//.equ GPIO_OUT_SET_R, GPIO_BASE + 0x014    // GPIO Output Register atomic set
+.equ GPIO_OE_R, GPIO_BASE + 0x020           // GPIO Output Enable Set Register
 
 .equ PIN, 25                                // Pin 25 if the LED on the Pico
-//.equ GPIO_OE_SET_R, GPIO_BASE + 0x024     // GPIO Output Enable Set Register
-//.equ GPIO_CTRL_R, GPIO_BASE + 0x018
 
 .equ GPIO_FUNC_SIO, 0x05 << 0                // Function Select: SIO=5
 .equ GPIO_OE_ENABLE_PIN, 1 << PIN
@@ -26,16 +22,16 @@ Vector_Table:
   .word     start + 1
 
 start:
-  // Enable GPIO Output for PIN
-  ldr r1, =GPIO_OE_R
-  ldr r2, =GPIO_OE_ENABLE_PIN
+  // Set Function Select as SIO
+  ldr r1, =GPIO_25_CTRL_R
+  ldr r2, =GPIO_FUNC_SIO
   ldr r0, [r1]
   orr r0, r0, r2
   str r0, [r1]
 
-  // Set Function Select as SIO
-  ldr r1, =GPIO_25_CTRL_R
-  ldr r2, =GPIO_FUNC_SIO
+  // Enable GPIO Output for PIN
+  ldr r1, =GPIO_OE_R
+  ldr r2, =GPIO_OE_ENABLE_PIN
   ldr r0, [r1]
   orr r0, r0, r2
   str r0, [r1]
