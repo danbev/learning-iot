7d153107be5440c900531cd2ebc9eae11c64bfb7
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-05-06 08:46:03 +0200

    Use non-atomic registers

--- rp/led/led-ext.s
+++ rp/led/led-ext.s
@@ -4,38 +4,45 @@
 
 .global start
 
-.equ GPIO_BASE, 0x40014000           // Base address of User Bank IO
-.equ GPIO_OUT_R, GPIO_BASE + 0x010   // GPIO Output Register
+.equ USER_BANK0_BASE, 0x40014000
+.equ GPIO_25_CTRL_R, USER_BANK0_BASE + 0x0CC // Control register for Pin 25
 
-.equ GPIO_OE_R, GPIO_BASE + 0x020    // GPIO Output Enable Register
-.equ GPIO0_CTRL_R, GPIO_BASE + 0x004
+.equ GPIO_BASE, 0xd0000000                   // SIO base
+.equ GPIO_OE_R, GPIO_BASE + 0x020           // GPIO Output Enable Set Register
+.equ GPIO_OUT_R, GPIO_BASE + 0x010          // GPIO Output Register
 
-.equ GPIO_FUNC_SIO, 0x5              // Function Select: SIO
-.equ GPIO_OE_ENABLE_PIN0, 1
-.equ GPIO_OUT_PIN0, 1 << 0
+//.equ GPIO_OUT_SET_R, GPIO_BASE + 0x014    // GPIO Output Register atomic set
+
+.equ PIN, 25                                // Pin 25 if the LED on the Pico
+//.equ GPIO_OE_SET_R, GPIO_BASE + 0x024     // GPIO Output Enable Set Register
+//.equ GPIO_CTRL_R, GPIO_BASE + 0x018
+
+.equ GPIO_FUNC_SIO, 0x05 << 0                // Function Select: SIO=5
+.equ GPIO_OE_ENABLE_PIN, 1 << PIN
+.equ GPIO_OUT_PIN, 1 << PIN                 
 
 Vector_Table:
   .word     0x20000000
   .word     start + 1
 
 start:
-  // Enable GPIO Output for GPIO0
+  // Enable GPIO Output for PIN
   ldr r1, =GPIO_OE_R
-  ldr r2, =GPIO_OE_ENABLE_PIN0
+  ldr r2, =GPIO_OE_ENABLE_PIN
   ldr r0, [r1]
   orr r0, r0, r2
   str r0, [r1]
 
   // Set Function Select as SIO
-  ldr r1, =GPIO0_CTRL_R
+  ldr r1, =GPIO_25_CTRL_R
   ldr r2, =GPIO_FUNC_SIO
   ldr r0, [r1]
   orr r0, r0, r2
   str r0, [r1]
 
-  // Set output of PIN 0 to high
+  // Set output of PIN to high
   ldr r1, =GPIO_OUT_R
-  ldr r2, =GPIO_OUT_PIN0
+  ldr r2, =GPIO_OUT_PIN
   ldr r0, [r1]
   orr r0, r0, r2
   str r0, [r1]
