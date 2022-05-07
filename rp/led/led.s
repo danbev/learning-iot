7d83ed57603c19402e6b54ddd0a3253f8a1b37e1
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-05-07 16:12:48 +0200

    Fix comment indentation in led.s

--- rp/led/led.s
+++ rp/led/led.s
@@ -8,14 +8,14 @@
 .equ GPIO_25_CTRL_R, USER_BANK0_BASE + 0x0CC // Control register for Pin 25
 
 .equ GPIO_BASE, 0xd0000000                   // SIO base
-.equ GPIO_OUT_R, GPIO_BASE + 0x010          // GPIO Output Register
-.equ GPIO_OE_R, GPIO_BASE + 0x020           // GPIO Output Enable Set Register
+.equ GPIO_OUT_R, GPIO_BASE + 0x010           // GPIO Output Register
+.equ GPIO_OE_R, GPIO_BASE + 0x020            // GPIO Output Enable Set Register
 
-.equ PIN, 25                                // Pin 25 if the LED on the Pico
+.equ PIN, 25                                 // Pin 25 if the LED on the Pico
 
 .equ GPIO_FUNC_SIO, 0x05 << 0                // Function Select: SIO=5
 .equ GPIO_OE_ENABLE_PIN, 1 << PIN
-.equ GPIO_OUT_PIN, 1 << PIN                 
+.equ GPIO_OUT_PIN, 1 << PIN
 
 Vector_Table:
   .word     0x20000000
